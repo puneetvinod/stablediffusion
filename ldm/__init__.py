33bd10c54a2f846a627ffc9d4fa78d62e47ee326
Puneet <puneetvinod@gmail.com>
2025-11-28 17:42:46 +0530

    fixes

---
+++ ldm/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for the `ldm` modules.
+
+This file makes `ldm` a regular Python package so it can be imported
+when the project is installed in editable mode or when running scripts.
+"""
+
+__all__ = []
+
+__version__ = "0.0.1"
